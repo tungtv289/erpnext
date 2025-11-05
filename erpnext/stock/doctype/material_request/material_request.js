5edcd448a8d020608ad6fd70f25d565d7e57afd9
CN Tinh Thanh Hoa <Dien toan@3500-WS000250.corp.agribank.com.vn>
2025-11-05 11:05:03 +0700

    Test deploy

--- erpnext/stock/doctype/material_request/material_request.js
+++ erpnext/stock/doctype/material_request/material_request.js
@@ -105,13 +105,13 @@ frappe.ui.form.on("Material Request", {
 	},
 
 	make_custom_buttons: function (frm) {
-		if (frm.doc.docstatus == 0) {
-			frm.add_custom_button(
-				__("Bill of Materials"),
-				() => frm.events.get_items_from_bom(frm),
-				__("Get Items From")
-			);
-		}
+		// if (frm.doc.docstatus == 0) {
+		// 	frm.add_custom_button(
+		// 		__("Bill of Materials"),
+		// 		() => frm.events.get_items_from_bom(frm),
+		// 		__("Get Items From")
+		// 	);
+		// }
 
 		if (frm.doc.docstatus == 1 && frm.doc.status != "Stopped") {
 			let precision = frappe.defaults.get_default("float_precision");
